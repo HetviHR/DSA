dc49499378593d44bc1b480daa27fe04abdc7e4f
Hetvi H Radadiya <146551630+HetviHR@users.noreply.github.com>
2024-07-22 16:10:58 +0530

    Add files via upload

--- countingSort.cpp
+++ countingSort.cpp
@@ -1,38 +1,44 @@
 #include<iostream>
-
 using namespace std;
+
+
 
 int main(){
 
-     int a[] = {5,8,3,4,2,0,4,2,2,1};
-     int max = a[0];
-     for(int i=0;i<10;i++)
-     {
-         if(a[i]>max){
+        int a[] = {5,8,3,4,2,0,4,2,2,1};
+        int max = a[0];
+        cout<<"original array is: ";
+
+        for(int h=0;h<10;h++){
+            cout<<a[h]<<"  ";
+        }
+        cout<<"\n";
+        for(int i=0;i<10;i++)
+        {
+            if(a[i]>max){
             max = a[i];
-         }
-     }
+            }
+        }
     cout<<"Maximum element in array is: "<<max<<endl;
 
+        //initialize with zero
     int count[max+1];
 
     for(int i=0;i<=max;i++){
-       count[i] = 0;
+        count[i] = 0;
         }
 
-        for(int i=0;i<=max;i++)
-        {
-            for(int j=0;j<10;j++){
-                    if(i == a[j])
-                    count[i]++;
+        //frequency
+        
+            for(int i=0;i<10;i++){
+                    count[a[i]]++;
             }
-        }
+        
 
         for(int h=0;h<=max;h++){
             cout<<count[h]<<"  ";
         }
         cout<<endl;
-        cout<<count[0]<<endl;
         int sum[max];
 
         for(int i=0;i<=max;i++){
@@ -40,14 +46,29 @@ int main(){
         }
         sum[0] = count[0];
         cout<<endl;
-        cout<<"i'm in sum"<< count[0];
+
         for(int i=1;i<=max;i++){
-           sum[i] = sum[i-1] + count[i] ;
+            sum[i] = sum[i-1] + count[i] ;
         }
+        //print sum
         for(int h=0;h<=max;h++){
             cout<<sum[h]<<"  ";
         }
 
+    //new output arrY
+    int output[10];
+    for (int i = 0; i < 10; i++)
+    {
+        output[sum[a[i]]-1]=a[i];
+        sum[a[i]]--;
+    }
+cout<<endl;
+    //print output
+    for(int h=0;h<10;h++){
+        
+            cout<<output[h]<<"  ";
+        }
+    
 
     }
 
